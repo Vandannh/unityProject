5c878975445c14d63646eac7d5a14f62d2fda28d
MacTheHack <Markus.masalkovski@gmail.com>
2021-04-27 15:31:24 +0200

    Added coin pickup functionalitty

--- TestProject/Assets/Scripts/CoinScript.cs
+++ TestProject/Assets/Scripts/CoinScript.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
+    ScoreScript scoreScript;
 
+    private void Start()
+    {
+        scoreScript = GetComponent<ScoreScript>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            scoreScript.coinCollected();
             Destroy(gameObject);
         }
     }
