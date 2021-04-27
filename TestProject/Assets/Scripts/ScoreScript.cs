b40fa2abcbc522cd44b8a5e6d9685e1d6bf102d8
MacTheHack <Markus.masalkovski@gmail.com>
2021-04-27 14:33:32 +0200

    Added a ui score counter

---
+++ TestProject/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+    [SerializeField] private Text scoreText;
+    private int score = 0;
+
+    void Start()
+    {
+        scoreText.text = "Gold Collected: " + score;  
+    }
+    public void coinCollected()
+    {
+        score++;
+        scoreText.text = "Gold Collected: " + score;
+    }
+
+    
+}
