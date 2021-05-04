b4684eead42824c3598e7cfa27e3e9d060c53144
MacTheHack <Markus.masalkovski@gmail.com>
2021-05-04 14:05:22 +0200

    Next level working (#hack)

--- TestProject/Assets/Scripts/ChangeLevelScript.cs
+++ TestProject/Assets/Scripts/ChangeLevelScript.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ChangeLevelScript : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
-            SceneManager.LoadScene(sceneName: "Level2");
+            SceneManager.LoadScene(sceneName: "Level_2");
         }
     }
-}
+        
+ }
+
